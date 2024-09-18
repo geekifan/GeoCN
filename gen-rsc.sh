e36676b78c2b77a4dc3300c447794e96be512231
iFan <74950140+geekifan@users.noreply.github.com>
2024-09-18 22:35:00 +0800

    Update gen-rsc.sh

--- gen-rsc.sh
+++ gen-rsc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo -e '/log info "Loading CN ipv4 address list"
+echo '/log info "Loading CN ipv4 address list"
 /ip firewall address-list remove [/ip firewall address-list find list=CN]
 /ip firewall address-list' > update-geoip.rsc
 cat GeoCN.txt | sed 's/^/:do { add address=/g' | sed 's/$/ list=CN } on-error={}/g' >> update-geoip.rsc
